85a6867eab6402165f91e310aed43bdcfdf7348f
DESKTOP-PK64GF2\hkristanto <harris.h-sse2015@tggs-bangkok.org>
2017-06-14 17:53:56 +0200

    4d6969554c07dfee8fb09090923d8fd577aeb1d4

--- modules/src/main/java/org/archive/modules/writer/WriterPoolProcessor.java
+++ modules/src/main/java/org/archive/modules/writer/WriterPoolProcessor.java
@@ -42,6 +42,7 @@ import org.archive.modules.CrawlMetadata;
 import org.archive.modules.CrawlURI;
 import org.archive.modules.ProcessResult;
 import org.archive.modules.Processor;
+import org.archive.modules.deciderules.DecideResult;
 import org.archive.modules.deciderules.recrawl.IdenticalDigestDecideRule;
 import org.archive.modules.net.CrawlHost;
 import org.archive.modules.net.ServerCache;
@@ -367,6 +368,12 @@ implements Lifecycle, Checkpointable, WriterPoolSettings {
             return false;
         }
         
+        if (getShouldProcessRule().decisionFor(curi) == DecideResult.REJECT) {
+            curi.getAnnotations().add(ANNOTATION_UNWRITTEN + ":rejected("
+                    + getShouldProcessRule().getClass() + ")");
+            return false;
+        }
+        
         return true; 
     }
     
@@ -454,9 +461,7 @@ implements Lifecycle, Checkpointable, WriterPoolSettings {
             File f = path.getFile();
             if (!f.exists()) {
                 try {
-                    synchronized (this) {
-                        FileUtils.ensureWriteableDirectory(f);
-                    }
+                    FileUtils.ensureWriteableDirectory(f);
                 } catch (Exception e) {
                     e.printStackTrace();
                     continue;
